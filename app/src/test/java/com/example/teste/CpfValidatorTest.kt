18666f33629c6d2a7726a53e3e6aad9d9da94914
gustavo.ppardini <ggpardini@gmail.com>
2025-10-30 12:04:45 -0300

    adicionando testes de cpf

--- app/src/test/java/com/example/teste/CpfValidatorTest.kt
+++ app/src/test/java/com/example/teste/CpfValidatorTest.kt
@@ -8,12 +8,52 @@ class CpfValidatorTest {
     @Test
     fun shouldReturnTrueWhenCpfIsValid(){
         val cpfValidator = CpfValidator()
-        val cpf = "529.982.247-25"
+        val cpf = "52998224725"
 
         val valid = cpfValidator.isValid(cpf)
 
         assertEquals(true, valid)
 
+    }
+
+    @Test
+    fun shouldReturnFalseForCpfWithAllSameDigits(){
+        val cpfValidator = CpfValidator()
+        val cpf = "000.000.000-00"
+
+        val valid = cpfValidator.isValid(cpf)
+
+        assertEquals(false, valid)
+    }
+
+    @Test
+    fun shouldReturnFalseForCpfWithInvalidLength(){
+        val cpfValidator = CpfValidator()
+        val cpf = "000.000.0-00"
+
+        val valid = cpfValidator.isValid(cpf)
+
+        assertEquals(false, valid)
+    }
+
+    @Test
+    fun shouldReturnFalseForCpfWithWrongVerifierDigits(){
+        val cpfValidator = CpfValidator()
+        val cpf = "000.000.000-44"
+
+        val valid = cpfValidator.isValid(cpf)
+
+        assertEquals(false, valid)
+    }
+
+    @Test
+    fun shouldReturnTrueForCpfWithPoints(){
+        val cpfValidator = CpfValidator()
+        val cpf = "529.982.247-25"
+
+        val valid = cpfValidator.isValid(cpf)
+
+        assertEquals(true, valid)
     }
 
 }
